62ac76a1a2b5fc17e3179cb5ccae6c98eb15a3c2
Adam Shimali <adam.shimali@gmail.com>
2021-05-19 16:39:35 +0100

    Added first base template and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+import glob
+
+from setuptools import setup
+
+
+setup(
+    name="design-history-template",
+    version="0.1.0",
+    description="Base templates for design history prototypes",
+    url="https://github.com/test-and-trace/design-history-template",
+    author="Adam Shimali",
+    license="MIT",
+    packages=["design_history_template"],
+    package_data={"design_history_template": ["templates/**/*.html"]},
+    install_requires=["jinja2"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
